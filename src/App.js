8ed46338263e9e45135f3c36cc1be28a43b63459
ghost_32 <kalash.strt@gmail.com>
2022-01-19 15:57:13 +0530

    temporary routing is done in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
 
+// importing components
+import Navbar from './components/Navbar';
+
+import { 
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+function Home(){
+  return(
+    <h1>Home</h1>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path = '/' element = { <Home /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
