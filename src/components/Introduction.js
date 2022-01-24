8e9e36ccc8da24fc2c90f5ec0f86a5b77b1fded5
ghost_32 <kalash.strt@gmail.com>
2022-01-24 21:53:10 +0530

    introduction section of home is created

---
+++ src/components/Introduction.js
@@ -0,0 +1,29 @@
+import './Introduction.css';
+
+function Introduction(){
+    return(
+        <>
+            <div className = 'introduction py-4 px-6'>
+                <div className = 'p-4 text-center'>
+                    <div className = "introduction__title mb-6">
+                        Designer & Web Developer
+                    </div>
+
+                    <div className = "introduction__subtitle mb-6">
+                        I love to develop and build things.
+                    </div>
+
+                    <div className = "my-8 flex justify-center items-center introduction__illustration">
+                        <img width = "200px" height = "200px" src = '/boy.png' alt = 'boy' />
+                    </div>
+                </div>
+
+                <div className = 'illustration px-6 flex justify-center items-center'>
+                    <img src = '/working.svg' alt = 'working' />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Introduction;
