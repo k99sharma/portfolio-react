2421ee5d2997861de3743892c69f40ff15039807
Kalash Sharma <kalash.strt@gmail.com>
2023-06-23 16:05:00 +0530

    add: divider component is added

---
+++ src/components/Divider/Divider.jsx
@@ -0,0 +1,8 @@
+// divider component
+export default function Divider(){
+    return(
+        <div className="divider my-14 flex items-center justify-center">
+            <hr className="border-black w-16" />
+        </div>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,13 @@
+import Divider from "../components/Divider/Divider.jsx";
+
 // home component
-export default function Home(){
-    return(
-        <div className="home">
-            Home
-        </div>
-    )
+export default function Home() {
+  return (
+    <div className="home">
+      Home
+      <div className="home__end">
+        <Divider />
+      </div>
+    </div>
+  );
 }
