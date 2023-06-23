044f2c237d1d2da267f42c9dd3182df8f61ac566
Kalash Sharma <kalash.strt@gmail.com>
2023-06-23 16:32:46 +0530

    add: home page is created
     added content in home page
    
     - social link icons are added
    
     todo: latest blog listing on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,70 @@
+// importing components
 import Divider from "../components/Divider/Divider.jsx";
+
+const socials = [
+  {
+    label: "github",
+    path: "#",
+    img: "/assets/github.png",
+  },
+  {
+    label: "linkedin",
+    path: "#",
+    img: "/assets/linkedin.png",
+  },
+  {
+    label: "blog",
+    path: "#",
+    img: "/assets/blog.png",
+  },
+];
 
 // home component
 export default function Home() {
   return (
     <div className="home">
-      Home
+      <div className="home__header">
+        <div className="home__header__heading text-4xl font-bold mb-1">
+          Kalash Sharma
+        </div>
+        <div className="home__header__subheading mb-2">
+          developer · mentor · blogger
+        </div>
+        <div className="home__header__introduction text-lg my-5">
+          I 💚 to develop & build things.
+        </div>
+      </div>
+
+      <div className="home__social mb-10">
+        {socials.map((social) => {
+          return (
+            <button
+              className="socialButton mr-5"
+              key={social.label}
+              type="button"
+              role="button"
+            >
+              <a href={social.path} target="_blank">
+                <img
+                  width={20}
+                  height={20}
+                  src={social.img}
+                  alt={social.label}
+                />
+              </a>
+            </button>
+          );
+        })}
+      </div>
+
+      <div className="home__blog">
+        <div className="home__blog__header my-5 font-semibold">
+          LATEST BLOGS
+        </div>
+
+        <div className="home__blog__"></div>
+      </div>
+
       <div className="home__end">
         <Divider />
       </div>
