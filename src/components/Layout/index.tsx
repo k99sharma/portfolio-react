eeb9cd18285c6ee3371e8e6b16c624c4d5213bea
Kalash Sharma <kalash.strt@gmail.com>
2023-07-15 00:08:43 +0530

    feat(page): added open source contribution page
     added open source contribution page
    
     - route is added
    
     add: user text selection color is changed to match the website theme
    
     todo: designing and data integration from GitHub

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -9,7 +9,7 @@ import { LayoutPropType } from "../../types/types";
 // layout component
 export default function Layout({ children }: LayoutPropType) {
   return (
-    <div className="layout flex justify-center bg-gradient-to-b from-bg-1 to-bg-2 min-h-screen">
+    <div className="layout flex justify-center bg-gradient-to-b from-bg-1 to-bg-2 min-h-screen user-select:text-red-300">
       <div className="w-5/6 md:w-4/6 lg:w-3/6">
         <Navbar />
         {children}
