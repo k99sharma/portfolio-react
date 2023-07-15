fb465b2ccd0400e3fa68935fbcd58b49b00e6126
Kalash Sharma <kalash.strt@gmail.com>
2023-07-15 21:46:51 +0530

    Update requestWrapper.ts
    need to delete this file

--- src/utils/requestWrapper.ts
+++ src/utils/requestWrapper.ts
@@ -1,15 +1,1 @@
-/**
- * Request wrapper generic function
- */
-export async function request<TResponse>(
-    url: string, 
-    config: RequestInit={}
-    ): Promise<TResponse>{
-    try {
-        const res = await fetch(url, config);
-        return await res.json();
-    } catch (err) {
-        console.error(err);
-        return { blogs: [], error: false };
-    }
-}
+
