9df1852e37f7d97f726fe8135b83fbe1c93fb673
Kalash Sharma <kalash.strt@gmail.com>
2023-08-05 11:57:57 +0530

    add: required libraries
     added libraries
    
     - material ui
     - tailwind css
     - radix icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
+// importing css
+import './index.css'
+
+// imports
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
