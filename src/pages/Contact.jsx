a36426562aa91f9c83da844e1ba68c0a747a4a62
Kalash Sharma <kalash.strt@gmail.com>
2023-08-05 21:02:16 +0530

    feat: contact page is designed
     added contact page form
    
     - netlify is configured for contact form
     - `TextField` component is used to create form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,72 @@
+// imports
+import { TextField } from "@mui/material";
+
 // contact page
-export default function Contact(){
-    return(
-        <div className="contact">
-            Contact Page
+export default function Contact() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="contact w-4/5">
+        <div className="contact__header text-2xl font-bold mb-14">
+          Love to hear from you, <br />
+          Get in touch
         </div>
-    )
+
+        <form name="contact" data-netlify="true" method="POST">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:mb-5">
+            <div className="mb-5">
+              <div className="form__label mb-3 font-semibold">Your Name</div>
+
+              <div className="form__input">
+                <TextField
+                  name="name"
+                  type="text"
+                  placeholder="jon doe"
+                  required
+                  aria-label="your name"
+                />
+              </div>
+            </div>
+
+            <div className="mb-5">
+              <div className="form__label mb-3 font-semibold">Your Email</div>
+
+              <div className="form__input">
+                <TextField
+                  name="email"
+                  type="email"
+                  placeholder="jondoe@email.com"
+                  aria-label="your email"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-10">
+            <div className="form__label mb-3 font-semibold">Message</div>
+
+            <div className="form__input">
+              <TextField
+                className="w-3/5 md:w-2/5 lg:w-2/5"
+                multiline
+                rows={5}
+                placeholder="Your Message ..."
+                aria-label="your message"
+                required
+              />
+            </div>
+          </div>
+
+          <button
+            className="bg-neutral-1 text-neutral-0 h-12 w-44 rounded-lg hover:shadow-lg"
+            role="button"
+            type="submit"
+            aria-label="form submit button"
+          >
+            Just Send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/utils/projects.js
+++ src/utils/projects.js
@@ -33,6 +33,28 @@ const projects = [
     img: "/projects/project.png",
     tech: "tech1, tech2, tech3",
   },
+  {
+    id: 3,
+    label: "Projec title",
+    description: "liajoeoj   aojeffoij aiwjef oj aeiofjaoijf aoiwfjioj",
+    social: {
+      github: "#",
+      live: "#",
+    },
+    img: "/projects/project.png",
+    tech: "tech1, tech2, tech3",
+  },
+  {
+    id: 4,
+    label: "Projec title",
+    description: "liajoeoj   aojeffoij aiwjef oj aeiofjaoijf aoiwfjioj",
+    social: {
+      github: "#",
+      live: "#",
+    },
+    img: "/projects/project.png",
+    tech: "tech1, tech2, tech3",
+  },
 ];
 
 export default projects;
