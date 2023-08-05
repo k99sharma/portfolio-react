df2fad8a4476297ba526b521fc7510f779e4c5f7
Kalash Sharma <kalash.strt@gmail.com>
2023-08-05 22:56:13 +0530

    add:  component is added in layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,11 +1,25 @@
 // importing components
 import Navbar from "../Navbar";
 import Footer from "../Footer";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+// scroll to top
+function ScrollToTopOnMount() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 // layout component
 export default function Layout({ children }) {
   return (
     <div className="layout font-sans">
+      <ScrollToTopOnMount />
       <div className="layout__navbar flex items-center justify-center py-5 mb-10">
         <Navbar />
       </div>
