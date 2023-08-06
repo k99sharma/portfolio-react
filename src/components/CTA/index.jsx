116b1b47302b038647e9591721aee98bf5b24157
Kalash Sharma <kalash.strt@gmail.com>
2023-08-06 11:00:55 +0530

    add: project navigation in mobile view

--- src/components/CTA/index.jsx
+++ src/components/CTA/index.jsx
@@ -7,14 +7,16 @@ export default function CTA() {
       <div className="cta__heading font-bold text-3xl">Get In Touch</div>
 
       <div className="cta__subheading font-semibold text-sm md:text-md lg:text-lg my-8 md:my-5">
-        {`Message me if you have any work <br /> opportunities or a 'cool' project
-        idea!`}
+        <p>
+          Message me if you have any work <br /> opportunities or a cool project
+          idea!
+        </p>
       </div>
 
       <div className="cta__button">
         <Link to="/contact">
           <button
-            className="border-neutral-1 border-2 font-semibold hover:bg-neutral-1 hover:text-neutral-0 px-10 py-4 rounded-3xl"
+            className="border-neutral-1 border-2 font-semibold hover:bg-neutral-1 hover:text-neutral-0 px-5 md:px-10 py-4 rounded-3xl"
             type="button"
             role="navigation"
             aria-label="cta"
