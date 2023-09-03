01bf3a8e8ab9db0e53d255494de029f154c8e1f5
Kalash Sharma <kalash.strt@gmail.com>
2023-09-03 21:17:26 +0530

    fix: action set to /

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -34,7 +34,7 @@ export default function Contact() {
           </div>
 
           <form
-            action="/contact"
+            action="/"
             autoComplete="off"
             name="contact"
             data-netlify="true"
